3a63770336fa0ae379a7b5c2f2116f8b4acd0bbb
Rawanabdelwahab <https://github.com/Rawanabdelwahab>
2024-12-16 22:00:18 +0200

    second

--- src/components/MainCards.jsx
+++ src/components/MainCards.jsx
@@ -1,5 +1,79 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
+// import * as React from "react";
+// import Card from "@mui/material/Card";
+// import CardContent from "@mui/material/CardContent";
+// import CardMedia from "@mui/material/CardMedia";
+// import Typography from "@mui/material/Typography";
+// import CardActionArea from "@mui/material/CardActionArea";
+// import Grid from "@mui/material/Grid2";
+
+// export default function ActionAreaCard({ name, title, email, src }) {
+
+//   return (
+//     <Grid container spacing={3}>
+//       {/* Card 1 */}
+//       <Grid item xs={12} sm={6} md={4}>
+//         <Card sx={{ maxWidth: 345 }}>
+//           <CardActionArea>
+//             <CardMedia component="img" height="140" image={src} alt={name} />
+//             <CardContent>
+//               <Typography gutterBottom variant="h5" component="div">
+//                 {name}
+//               </Typography>
+//               <Typography variant="body2" sx={{ color: "text.secondary" }}>
+//                 {title}
+//               </Typography>
+//               <Typography variant="body2" sx={{ color: "text.secondary" }}>
+//                 {email}
+//               </Typography>
+//             </CardContent>
+//           </CardActionArea>
+//         </Card>
+//       </Grid>
+//       {/* Card 2 */}
+//       <Grid item xs={12} sm={6} md={4}>
+//         <Card sx={{ maxWidth: 345 }}>
+//           <CardActionArea>
+//             <CardMedia component="img" height="140" image={src} alt={name} />
+//             <CardContent>
+//               <Typography gutterBottom variant="h5" component="div">
+//                 {name}
+//               </Typography>
+//               <Typography variant="body2" sx={{ color: "text.secondary" }}>
+//                 {title}
+//               </Typography>
+//               <Typography variant="body2" sx={{ color: "text.secondary" }}>
+//                 {email}
+//               </Typography>
+//             </CardContent>
+//           </CardActionArea>
+//         </Card>
+//       </Grid>
+
+//       {/* Card 3 */}
+//       <Grid item xs={12} sm={6} md={4}>
+//         <Card sx={{ maxWidth: 345 }}>
+//           <CardActionArea>
+//             <CardMedia component="img" height="140" image={src} alt={name} />
+//             <CardContent>
+//               <Typography gutterBottom variant="h5" component="div">
+//                 {name}
+//               </Typography>
+//               <Typography variant="body2" sx={{ color: "text.secondary" }}>
+//                 {title}
+//               </Typography>
+//               <Typography variant="body2" sx={{ color: "text.secondary" }}>
+//                 {email}
+//               </Typography>
+//             </CardContent>
+//           </CardActionArea>
+//         </Card>
+//       </Grid>
+//     </Grid>
+//   );
+// }
+
 import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -8,67 +82,29 @@ import Typography from "@mui/material/Typography";
 import CardActionArea from "@mui/material/CardActionArea";
 import Grid from "@mui/material/Grid2";
 
-export default function ActionAreaCard({ name, title, email, src }) {
+export default function MainCards({ cards }) {
   return (
     <Grid container spacing={3}>
-      {/* Card 1 */}
-      <Grid item xs={12} sm={6} md={4}>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <CardMedia component="img" height="140" image={src} alt={name} />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {name}
-              </Typography>
-              <Typography variant="body2" sx={{ color: "text.secondary" }}>
-                {title}
-              </Typography>
-              <Typography variant="body2" sx={{ color: "text.secondary" }}>
-                {email}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </Grid>
-      {/* Card 2 */}
-      <Grid item xs={12} sm={6} md={4}>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <CardMedia component="img" height="140" image={src} alt={name} />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {name}
-              </Typography>
-              <Typography variant="body2" sx={{ color: "text.secondary" }}>
-                {title}
-              </Typography>
-              <Typography variant="body2" sx={{ color: "text.secondary" }}>
-                {email}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </Grid>
-
-      {/* Card 3 */}
-      <Grid item xs={12} sm={6} md={4}>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <CardMedia component="img" height="140" image={src} alt={name} />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {name}
-              </Typography>
-              <Typography variant="body2" sx={{ color: "text.secondary" }}>
-                {title}
-              </Typography>
-              <Typography variant="body2" sx={{ color: "text.secondary" }}>
-                {email}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </Grid>
+      {cards.map((card, index) => (
+        <Grid item xs={4} sm={4} md={4} key={index}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+              <CardMedia component="img" height="140" image={card.src} alt={card.name} />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {card.name}
+                </Typography>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  {card.title}
+                </Typography>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  {card.email}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      ))}
     </Grid>
   );
 }
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,15 +1,43 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import MainCards from "./MainCards.jsx";
+import MainCards from "./MainCards";
 import template from "../images/template-3.jpg";
+
 export default function MainContent() {
-  return (
-    <div>
-     <MainCards
-            name="Hilton Alexandria Corniche "
-            title="Hilton Alexandria Corniche"
-            src={template}
-          />
-    </div>
-  );
+  // Array of card data
+  const cardData = [
+    {
+      name: "Hilton Alexandria Corniche",
+      title: "Luxury at its best",
+      email: "contact@hilton.com",
+      src: template,
+    },
+    {
+      name: "Four Seasons Hotel Alexandria",
+      title: "Elegance and Comfort",
+      email: "info@fourseasons.com",
+      src: template,
+    },
+    {
+      name: "Sheraton Montazah Hotel",
+      title: "Sea View Bliss",
+      email: "reservations@sheraton.com",
+      src: template,
+    },
+    {
+      name: "Sheraton Montazah Hotel",
+      title: "Sea View Bliss",
+      email: "reservations@sheraton.com",
+      src: template,
+    },
+    {
+      name: "Sheraton Montazah Hotel",
+      title: "Sea View Bliss",
+      email: "reservations@sheraton.com",
+      src: template,
+    },
+  ];
+
+  return <MainCards cards={cardData} />;
 }
+
